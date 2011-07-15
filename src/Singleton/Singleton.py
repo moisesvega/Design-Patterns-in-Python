47927b98335ec582d362ef17a3188c237fc26d54
Moises Vega <moises@ubuntu.ubuntu-domain>
2011-07-15 00:46:48 -0500

    Add project and Singleton

---
+++ src/Singleton/Singleton.py
@@ -0,0 +1,36 @@
+'''
+Created on Jul 14, 2011
+
+@author: moises.vega
+'''
+
+class Singleton:
+    __single = None
+    def __init__( self ):
+        if Singleton.__single:
+            raise Singleton.__single
+        Singleton.__single = self
+
+class Child( Singleton ):
+    def __init__( self, name ):
+        Singleton.__init__( self )
+        self.__name = name
+    def name( self ):
+        return self.__name
+
+class Junior( Singleton ):
+    pass
+
+def Handle( x = Singleton ):
+    try:
+        single = x()
+    except Singleton, s:
+        single = s
+    return single
+
+
+
+child = Child( 'Moy' )
+junior = Handle( Junior )
+print( junior.name() )
+
